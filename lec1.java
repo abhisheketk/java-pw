f96ebd5934509fda3d61f4c87330ff76c53a47bf
abhisheketk <guptaabhishek1045@gmail.com>
2022-11-10 21:09:31 +0530

    first

---
+++ lec1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class lec1 {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        int a = sr.nextInt();
+        int b = sr.nextInt();
+        if (a == b) {
+            System.out.println("equal");
+        } else {
+            if (a > b) {
+                System.out.println("a is grater");
+            } else {
+                System.out.println("a is smaller");
+            }
+
+        }
+
+    }
+}
